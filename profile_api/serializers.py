cf3c49e6f7e2fe78c538d155341dd032eaa16dfc
prachi7057 <prachinema02@gmail.com>
2022-12-01 09:54:14 +0530

    Creating serializers for the POST request

---
+++ profile_api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class HelloSerializers(serializers.Serializer):
+
+    name=serializers.CharField(max_length=10)
+
+    age = serializers.IntegerField()
--- profile_api/views.py
+++ profile_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
-
 from rest_framework.response import Response
+from rest_framework import status
+from profile_api import serializers
 
 
 class HelloApiVIew(APIView):
@@ -21,3 +22,47 @@ class HelloApiVIew(APIView):
 
 
         return Response({"message":"hello world", "an_api" : an_api})
+
+
+    serializer_class = serializers.HelloSerializers
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+        print('Log for serializer',type(serializer))
+        if serializer.is_valid():
+            name=serializer.validated_data.get("name")
+
+            age=serializer.validated_data.get("age")
+
+            message = f"hello {name, age}"
+
+            return Response({'message' : message})
+
+        else:
+
+            return Response(serializer.errors,
+
+            status = status.HTTP_400_BAD_REQUEST
+
+            )
+
+
+
+    def put(self, request, pk=None):
+
+        """Handling update an object"""
+
+        return Response({'method':'PUT'})
+
+
+    def patch(self, request, pk=None):
+
+        """Handling partial update of an object """
+
+        return Response({'method' : 'PATCH'})
+
+
+    def delete(self, request,pk=None):
+
+        """Delete an Object """
+
+        return Response({'method' : 'DELETE'})
